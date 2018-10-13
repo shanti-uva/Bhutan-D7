3d351728ea69f870caf9ff547cd02a24b83a0453
Gerard Ketuma <gketuma@gmail.com>
2018-10-13 15:06:50 -0500

    Add Bhutan footer script. Fix errors in bhutan-search.js

--- sites/all/themes/sarvaka_bhutan/js/bhutan-search.js
+++ sites/all/themes/sarvaka_bhutan/js/bhutan-search.js
@@ -1,28 +1,57 @@
 (function ($) { // jQuery wrapper function
 	var bto;
 	Drupal.behaviors.bhutan_search_deafultOpen = {
-		attach: function (context, settings) {
-				if($(".flyout-open").length ) { 
-					$("#search-flyout").once('openflyout', function() {
-						$(this).openMbExtruder();
-						$('#kmaps-search .treeview a').click();
-						bto = setInterval(function() {
-							if($('#ajax-id-8260').length > 0) {
-								var topPos = document.getElementById('ajax-id-8260').offsetTop;
-								document.getElementById('tree').scrollTop = topPos;
-								window.clearInterval(bto);
-							}
-							if($('#ajax-id-427').length > 0) {
-								var topPos = document.getElementById('ajax-id-427').offsetTop;
-								document.getElementById('tree').scrollTop = topPos;
-								window.clearInterval(bto);
-							}  
-						}, 1000);
-						setTimeout(function() { window.clearInterval(bto); }, 15000);
-					});
-				}
-				
+		attach: function(context, settings) {
+			if($(".flyout-open").length ) {
+				$("#search-flyout").once('openflyout', function() {
+					$(this).openMbExtruder();
+					$('#kmaps-search .treeview a').click();
+					bto = setInterval(function() {
+						if($('#ajax-id-8260').length > 0) {
+							var topPos = document.getElementById('ajax-id-8260').offsetTop;
+							//The 'tree' element is not being found
+							//document.getElementById('tree').scrollTop = topPos;
+							window.clearInterval(bto);
+						}
+						if($('#ajax-id-427').length > 0) {
+							var topPos = document.getElementById('ajax-id-427').offsetTop;
+							//The 'tree' element is not being found
+							//document.getElementById('tree').scrollTop = topPos;
+							window.clearInterval(bto);
+						}
+					}, 1000);
+					setTimeout(function() { window.clearInterval(bto); }, 15000);
+				});
+			}
+		}
     }
-  };	
-  			
+
+    // Sidebar and footer coordinate heights
+    Drupal.behaviors.bhutanSidebarFooterGravity = {
+        attach: function (context, settings) {
+            Drupal.ShantiSarvaka.bhutanSidebarFooterGravity = function () {
+                console.log("Here we go again");
+                var height = $(window).height();
+                var mainwrapper_minimum = (height) - 190;
+                var mainwrapper_minimum_hastabs = (height) - 230;
+                var mainwrapper_minimum_hasadminmenu = (height) - 220;
+                var mainwrapper_minimum_hasadminfooter = (height) - 252;
+
+                mainwrapper_minimum = parseInt(mainwrapper_minimum) + 'px';
+                mainwrapper_minimum_hastabs = parseInt(mainwrapper_minimum_hastabs) + 'px';
+                mainwrapper_minimum_hasadminmenu = parseInt(mainwrapper_minimum_hasadminmenu) + 'px';
+                mainwrapper_minimum_hasadminfooter = parseInt(mainwrapper_minimum_hasadminfooter) + 'px';
+
+                $(".main-col").css('min-height', mainwrapper_minimum);
+                $(".has-tabs .main-col").css('min-height', mainwrapper_minimum_hastabs);
+                $(".admin-menu .main-col").css('min-height', mainwrapper_minimum_hasadminmenu);
+                $(".admin-menu.has-tabs .main-col").css('min-height', mainwrapper_minimum_hasadminfooter);
+            };
+
+            $(window).bind('load resize orientationchange', function () {
+                clearTimeout(this.id);
+                this.id = setTimeout(Drupal.ShantiSarvaka.bhutanSidebarFooterGravity, 300);
+            });
+        }
+    };
 } (jQuery));
